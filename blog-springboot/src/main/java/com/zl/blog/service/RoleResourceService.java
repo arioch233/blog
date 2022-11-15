a64719212b3edb3742a3e06d419a2ad5964a4946
admin <1738252674@qq.com>
2022-11-15 10:55:51 +0800

    +: role resource module

---
+++ blog-springboot/src/main/java/com/zl/blog/service/RoleResourceService.java
@@ -0,0 +1,15 @@
+package com.zl.blog.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zl.blog.entity.RoleResource;
+
+/**
+ * 角色资源服务
+ *
+ * @author 冷血毒舌
+ * @description 针对表【sys_role_resource】的数据库操作Service
+ * @createDate 2022-11-15 10:43:34
+ */
+public interface RoleResourceService extends IService<RoleResource> {
+
+}
---
+++ blog-springboot/src/main/java/com/zl/blog/service/impl/RoleResourceServiceImpl.java
@@ -0,0 +1,24 @@
+package com.zl.blog.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zl.blog.entity.RoleResource;
+import com.zl.blog.mapper.RoleResourceMapper;
+import com.zl.blog.service.RoleResourceService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 角色资源服务实现
+ *
+ * @author 冷血毒舌
+ * @description 针对表【sys_role_resource】的数据库操作Service实现
+ * @createDate 2022-11-15 10:43:34
+ */
+@Service
+public class RoleResourceServiceImpl extends ServiceImpl<RoleResourceMapper, RoleResource>
+        implements RoleResourceService {
+
+}
+
+
+
+
