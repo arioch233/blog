9e672a2ea3da635003248b30ce263fabdcc5e216
admin <1738252674@qq.com>
2022-12-01 23:27:25 +0800

    +: add archive page

---
+++ blog-vue/blog/src/views/archive/ArchiveView.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <!-- banner -->
+    <div class="banner" :style="cover">
+      <h1 class="banner-title">归档</h1>
+    </div>
+    <!-- 归档列表 -->
+    <v-card class="blog-container">
+      <timeline>
+        <timeline-title> 目前共计{{ count }}篇文章</timeline-title>
+        <timeline-item v-for="item of archiveList" :key="item.id">
+          <!-- 日期 -->
+          <span class="time">{{ item.createTime | date }}</span>
+          <!-- 文章标题 -->
+          <router-link
+            :to="'/articles/' + item.id"
+            style="color: #666; text-decoration: none"
+          >
+            {{ item.articleTitle }}
+          </router-link>
+        </timeline-item>
+      </timeline>
+      <!-- 分页按钮 -->
+      <v-pagination
+        color="#00C4B6"
+        v-model="current"
+        :length="Math.ceil(count / 10)"
+        total-visible="7"
+      />
+    </v-card>
+  </div>
+</template>
+<script>
+import { Timeline, TimelineItem, TimelineTitle } from "vue-cute-timeline";
+
+export default {
+  name: "ArchiveView",
+  created() {
+    this.listArchives();
+  },
+  components: {
+    Timeline,
+    TimelineItem,
+    TimelineTitle,
+  },
+  data: function () {
+    return {
+      current: 1,
+      count: 0,
+      archiveList: [
+        {
+          createTime: "2022-07-01",
+          articleTitle: "测试标题1",
+        },
+        {
+          createTime: "2022-07-01",
+          articleTitle: "测试标题2",
+        },
+        {
+          createTime: "2022-07-01",
+          articleTitle: "测试标题3",
+        },
+      ],
+    };
+  },
+  methods: {
+    listArchives() {
+      this.request
+        .get("/article/archive", {
+          params: { current: this.current },
+        })
+        .then((data) => {
+          this.archiveList = data.data.recordList;
+          this.count = data.data.count;
+        });
+    },
+  },
+  computed: {
+    cover() {
+      let cover = "";
+      this.$store.state.blogInfo.pageList.forEach((item) => {
+        if (item.pageLabel === "archive") {
+          cover = item.pageCover;
+        }
+      });
+      return "background: url(" + cover + ") center center / cover no-repeat";
+    },
+  },
+  watch: {
+    current(value) {
+      this.request
+        .get("/article/archive", {
+          params: { current: value },
+        })
+        .then((data) => {
+          this.archiveList = data.data.recordList;
+          this.count = data.data.count;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.time {
+  font-size: 0.75rem;
+  color: #555;
+  margin-right: 1rem;
+}
+</style>
